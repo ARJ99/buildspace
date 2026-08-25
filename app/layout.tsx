2e72f66f3064301e7ee65058c59fecd9d7ace65f
PARIS <alejoriios14@gmail.com>
2026-08-25 10:42:36 -0500

    Foundation

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: "buildspace",
+  description: "Gamified learning platform for developers",
+};
+
+export default function RootLayout({ children }: LayoutProps<"/">) {
+  return (
+    <ClerkProvider>
+      <html
+        lang="en"
+        suppressHydrationWarning
+      >
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
